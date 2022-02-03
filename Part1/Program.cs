005b535c51b4ba0d263b8a9f5c2396bcbb6e51ae
machine-human <dhwltn02@gmail.com>
2022-02-03 22:10:08 +0900

    [part-2] 프로젝트 생성

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -1,62 +1,13 @@
 ﻿using System;
 
-namespace Part1
+namespace Part2
 {
-
-    // 객체는 속성과 기능으로 분류 가능
-
-    // Knight
-    // 속성 : hp, attack, pos
-    // 기능 : Move, Attack, Die
-
-    class Knight
+    class Program
     {
-        public int hp;
-        public int attack;
-
-        public void Move()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Knight Move");
-        }
+            Console.WriteLine("Hello world!!");
 
-        public void Attack()
-        {
-            Console.WriteLine("Knight Attack");
-        }
-    }
-
-    struct Mage
-    {
-        public int hp;
-        public int attack;
-
-
-        class Program
-        {
-
-            static void KillMage(Mage mage)
-            {
-                mage.hp = 0;
-            }
-
-            static void KillKnight(Knight knight)
-            {
-                knight.hp = 0;
-            }
-
-            static void Main(string[] args)
-            {
-
-                Mage mage;
-                mage.hp = 100;
-                mage.attack = 50;
-                KillMage(mage);
-
-                Knight knight = new Knight();
-                knight.hp = 100;
-                knight.attack = 10;
-                KillKnight(knight);
-            }
         }
     }
 }
