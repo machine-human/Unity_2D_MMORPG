7ce9b8e4d07ba8a96007cf33fc53fc1675b3429a
machine-human <dhwltn02@gmail.com>
2022-02-03 23:06:43 +0900

    환경 설정

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -6,8 +6,43 @@ namespace Part2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!!");
+            Console.CursorVisible = false;
 
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+            int lastTick = 0;
+
+            while(true)
+            {
+                // FPS 프레임 (60프레임 OK, 30프레임 이하로 NO)
+
+                #region 프레임 관리
+                // 만약에 경과한 시간이 1/30초보다 작다면
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+                #endregion
+
+                // 입력
+
+                // 로직
+
+                // 렌더링
+                Console.SetCursorPosition(0, 0);
+
+                for(int i = 0; i < 25; i++)
+                {
+                    for (int j=0; j <25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.Write(CIRCLE);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+           
         }
     }
 }
