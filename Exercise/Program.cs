0d33c42f3bf95743d7d61d94f83bcf3eb49f9910
machine-human <dhwltn02@gmail.com>
2022-03-02 18:49:36 +0900

    [part-2] 그래프 생성

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -2,6 +2,20 @@
 
 namespace Exercise
 {
+
+    class Graph
+    {
+        int[,] adj = new int[6, 6]
+        {
+            {0,0,0,0,0,0 },
+            {0,0,0,0,0,0 },
+            {0,0,0,0,0,0 },
+            {0,0,0,0,0,0 },
+            {0,0,0,0,0,0 },
+            {0,0,0,0,0,0 }
+        };
+    }
+
     class Program
     {
         static void Main(string[] args)
