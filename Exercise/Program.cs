cc3d163e056d4235db0c71a26d1d17cad4ace210
machine-human <dhwltn02@gmail.com>
2022-03-15 20:27:55 +0900

    [part-2] BFS (너비 우선 탐색)

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -6,7 +6,7 @@ namespace Exercise
 
     class Graph
     {
-        
+
         int[,] adj = new int[6, 6]
         {
             {0,1,0,1,0,0 }, // 1번 노드
@@ -27,51 +27,31 @@ namespace Exercise
             new List<int>() { 4 } // 1번 노드
         };
 
-        bool[] visited = new bool[6];
-
-        // 행렬(adj변수)을 이용한 DFS
-        // 1) 우선 now부터 방문하고,
-        // 2) now와 연결된 정점들을 하나씩 확인해서, 아직 미방문 상태라면 방문
-        public void DFS(int now)
+        public void BFS(int start)
         {
-            Console.WriteLine(now);
-            visited[now] = true; // 1) 우선 now부터 방문하고,
+            bool[] found = new bool[6];
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
 
-            for (int next = 0; next < 6; next++)
+            while(q.Count > 0)
             {
-                if (adj[now, next] == 0) // 연결되어 있지 않으면 스킵
-                    continue;
+                int now = q.Dequeue();
+                Console.WriteLine(now);
 
-                if (visited[next]) // 이미 방문했으면 스킵
-                    continue;
+                for(int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == 0) // 인접하지 않았으면 스킵
+                        continue;
+                    if (found[next]) // 이미 발견한 애라면 스킵
+                        continue;
 
-                DFS(next);
+                    q.Enqueue(next);
+                    found[next] = true;
+                }
             }
         }
 
-        // 리스트(adj2변수)을 이용한 DFS
-        public void DFS2(int now)
-        {
-            Console.WriteLine(now);
-            visited[now] = true; // 1) 우선 now부터 방문하고,
-          
-            foreach(int next in adj2[now])
-            {
-                if (visited[next]) // 이미 방문했으면 스킵
-                    continue;
-
-                DFS2(next);
-            }
-        }
-
-        // 만약 노드가 끊어졌을때 모든 노드를 찾을 수 있는 함ㄹ
-        public void SearchAll()
-        {
-            visited = new bool[6];
-            for (int now = 0; now < 6; now++)
-                if (visited[now] == false)
-                    DFS(now);
-        }
     }
 
     class Program
@@ -80,7 +60,7 @@ namespace Exercise
         {
             Console.WriteLine("Hello World!");
             Graph graph = new Graph();
-            graph.DFS2(3);
+            graph.BFS(0);
         }
     }
 }
